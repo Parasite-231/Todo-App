8f6944cebd5610ec7ca4d7bc56d36b8cce3606af
Parasite-231 <muktadir@iut-dhaka.edu>
2022-05-31 22:09:57 +0600

    init

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+const todoSchema = require("./schema/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
+
+const app = express();
+app.set("view engine", "ejs");
+dotenv.config();
+// request parsers
+// app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//database connection
+mongoose
+  .connect(process.env.APP_MONGO_CONNECTION, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected Successfully!!!"))
+  .catch((err) => console.log(err));
+
+//set Routers
+//get todos from database
+app.get("/", async (req, res) => {
+  const todos = await Todo.find({}).sort("-date");
+  res.render("index", { todos });
+});
+
+//post todos -- handleform`
+// app.post("/", async (req, res) => {
+//   const text = req.body.text.trim();
+//   if (text == "") {
+//     return res.redirect("/");
+//   }
+//   const newTodo = new Todo({
+//     text,
+//   });
+//   await newTodo.save();
+//   res.redirect("/");
+// });
+app.post("/", async (req, res) => {
+  //todo object that will insert into database
+  //new document object create
+  //single document object insert way
+  const text = req.body.text.trim();
+  const createNewTodo = new Todo({ text });
+  //actual document is created
+  //newTodo will enter in the database
+  await createNewTodo.save((err) => {
+    //save is the instance method
+    if (err) {
+      res.status(500).send("There was a server error in saving Todo");
+      res.end();
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+//initiate
+app.listen(process.env.PORT, () => {
+  console.log(` Listening to PORT : ${process.env.PORT}`);
+});
